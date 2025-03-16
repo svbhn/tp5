2da9277e89b709c68e4fcccf5414888015e33abf
svbhn <146323641+svbhn@users.noreply.github.com>
2025-03-16 19:17:07 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace tpmodul5_103022300081;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        HaloGeneric.SapaUser<string>("John");
+
+        DataGeneric<string> nim = new DataGeneric<string>("103022300081");
+        nim.PrintData();
+
+        Console.ReadLine(); 
+    }
+}
